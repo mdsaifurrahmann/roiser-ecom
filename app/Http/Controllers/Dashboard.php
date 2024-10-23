f09ec50b765db69a754b32d21a6fd717dd7199d8
mdsaifurrahmann <md.saifurrahmann029@gmail.com>
2024-10-24 05:17:07 +0600

    added: Dashboard widgets

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Products\Products;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
@@ -10,18 +12,26 @@ class Dashboard extends Controller
 {
     public function index()
     {
-//        $routes = Route::getRoutes()->getRoutesByName();
-//
-//        $routeNames = array_keys($routes);
-//
-//        $remove = ['home', 'contact', 'shop', 'register', 'login'];
-//
-//        $routeNames = array_diff($routeNames, $remove);
-//
-//        $routeNames = array_values($routeNames);
+        //        $routes = Route::getRoutes()->getRoutesByName();
+        //
+        //        $routeNames = array_keys($routes);
+        //
+        //        $remove = ['home', 'contact', 'shop', 'register', 'login'];
+        //
+        //        $routeNames = array_diff($routeNames, $remove);
+        //
+        //        $routeNames = array_values($routeNames);
 
 
-//        dd($routeNames);
-        return view('panel.dashboard');
+        //        dd($routeNames);
+
+        $members = User::count();
+        $activeProducts = Products::where('status', 1)->count();
+        $draftProducts = Products::where('status', 0)->count();
+        return view('panel.dashboard', [
+            'members' => $members,
+            'activeProducts' => $activeProducts,
+            'draftProducts' => $draftProducts
+        ]);
     }
 }
